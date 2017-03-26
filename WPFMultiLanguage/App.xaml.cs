84773301eb3098b8bbee04050fae07c0412f277b
kilosamagali <kmagali120@gmail.com>
2017-03-26 23:00:22 +0200

    Added stringresource files

--- WPFMultiLanguage/App.xaml.cs
+++ WPFMultiLanguage/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,48 @@ namespace WPFMultiLanguage
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SelectCulture("fr-FR");
+        }
+       
+            public static void SelectCulture(string culture)
+        {
+            if (String.IsNullOrEmpty(culture))
+                return;
+
+            //Copy all MergedDictionarys into a auxiliar list.
+            var dictionaryList = Application.Current.Resources.MergedDictionaries.ToList();
+
+            //Search for the specified culture.     
+            string requestedCulture = string.Format("StringResources.{0}.xaml", culture);
+            var resourceDictionary = dictionaryList.
+                FirstOrDefault(d => d.Source.OriginalString == requestedCulture);
+
+            if (resourceDictionary == null)
+            {
+                //If not found, select our default language.             
+                requestedCulture = "StringResources.xaml";
+                resourceDictionary = dictionaryList.
+                    FirstOrDefault(d => d.Source.OriginalString == requestedCulture);
+            }
+
+            //If we have the requested resource, remove it from the list and place at the end.     
+            //Then this language will be our string table to use.      
+            if (resourceDictionary != null)
+            {
+                Application.Current.Resources.MergedDictionaries.Remove(resourceDictionary);
+                Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            }
+
+            //Inform the threads of the new culture.     
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+
+        }
+
     }
+
+
 }
--- WPFMultiLanguage/MainWindow.xaml.cs
+++ WPFMultiLanguage/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,23 @@ namespace WPFMultiLanguage
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //string str = FindResource("language").ToString(); 
+            string str = Application.Current.Resources["language"] as string;
+            labelAngu.Content = str;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(CultureInfo.CurrentCulture.Name == "de-DE")
+                App.SelectCulture("en-EN");
+            else if (CultureInfo.CurrentCulture.Name == "en-EN")
+                App.SelectCulture("fr-FR");
+            else if (CultureInfo.CurrentCulture.Name == "fr-FR")
+                App.SelectCulture("de-DE");
         }
     }
 }
